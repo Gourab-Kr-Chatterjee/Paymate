c95fa73ca13f235b0ce58113d04abe6bbdfb514b
Gourab Chatterjee <114818987+Gourab-Kr-Chatterjee@users.noreply.github.com>
2025-01-07 14:43:26 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PayMate',
+  webDir: 'www'
+};
+
+export default config;
